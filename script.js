d64c1f4d5ef99e3e6104230dc882e62129875fc0
luigipr <luigiprofeti@gmail.com>
2023-04-03 14:49:25 -0300

    versão com o código mais limpo

--- script.js
+++ script.js
@@ -9,109 +9,63 @@ let soma = 0;
 
 
 function selecionarPrato(seletor){    
-    // primeiro vou verificar se existe botao já selecionado
-    console.log(seletor)
-    // buscando um botao com a classe selecionado
     const botaoSelecionadoAnteriormente = document.querySelector('.dishes .selecionado');
-    console.log(botaoSelecionadoAnteriormente);
-    // se existir botao selecionado, 
-    
     if ( botaoSelecionadoAnteriormente !== null ){
-        //remove a classe selecionado desse botao
         botaoSelecionadoAnteriormente.classList.remove('selecionado');
     }
-    // buscar o novo botqo clicado
     const botao = document.querySelector(seletor);
-    dish = botao.querySelector('.title').innerHTML
-    pricedish = botao.querySelector('.price').innerHTML
+    dish = botao.querySelector('.title').innerHTML;
+    pricedish = botao.querySelector('.price').innerHTML;
     pricedish = pricedish.slice(3, 8).replace(',','.')
-    console.log(pricedish)
-    // adiciona a classe selecionado
     botao.classList.toggle('selecionado');
-    dish = botao.querySelector('.title').innerHTML
-    console.log(dish)
+    dish = botao.querySelector('.title').innerHTML;
     displaybutton();
 }
 
 function selecionarBebida(seletor){    
-    // primeiro vou verificar se existe botao já selecionado
-    // buscando um botao com a classe selecionado
     const botaoSelecionadoAnteriormente = document.querySelector('.drinks .selecionado');
-    console.log(botaoSelecionadoAnteriormente);
-    // se existir botao selecionado, 
     if ( botaoSelecionadoAnteriormente !== null ){
         //remove a classe selecionado desse botao
         botaoSelecionadoAnteriormente.classList.remove('selecionado');
     }
-    // buscar o novo botqo clicado
     const botao = document.querySelector(seletor);
-    
-    // adiciona a classe selecionado
     botao.classList.add('selecionado');
-    drink = botao.querySelector('.title').innerHTML
-    pricedrink = botao.querySelector('.price').innerHTML
-    pricedrink = pricedrink.slice(3, 8).replace(',','.')
-    console.log(pricedrink)
-    console.log(drink)
+    drink = botao.querySelector('.title').innerHTML;
+    pricedrink = botao.querySelector('.price').innerHTML;
+    pricedrink = pricedrink.slice(3, 8).replace(',','.');
     displaybutton();
 }
 
 function selecionarSobremesa(seletor){    
-    // primeiro vou verificar se existe botao já selecionado
-    // buscando um botao com a classe selecionado
     const botaoSelecionadoAnteriormente = document.querySelector('.dessert.selecionado');
-    console.log(botaoSelecionadoAnteriormente);
-    // se existir botao selecionado, 
     if (botaoSelecionadoAnteriormente !== null){
-        //remove a classe selecionado desse botao
         botaoSelecionadoAnteriormente.classList.remove('selecionado');
     }
-    // buscar o novo botqo clicado
     const botao = document.querySelector(seletor);
-    
-    // adiciona a classe selecionado
     botao.classList.add('selecionado');
-    dessert = botao.querySelector('.title').innerHTML
-    pricedessert = botao.querySelector('.price').innerHTML
+    dessert = botao.querySelector('.title').innerHTML;
+    pricedessert = botao.querySelector('.price').innerHTML;
     pricedessert = pricedessert.slice(3, 8).replace(',','.')
-    console.log(pricedessert)
-
-    console.log(dessert)
     displaybutton();
 }
-/*
-function selecionarSobremesa(seletor){    
-    const botaoSelecionadoAnteriormente = document.querySelector('.desserts .selecionado');
-    console.log(botaoSelecionadoAnteriormente); 
-    if ( botaoSelecionadoAnteriormente !== null ){
-        botaoSelecionadoAnteriormente.classList.remove('.selecionado');
-    }
-
-    seletor.classList.add('selecionado');
-    dessert = seletor
-    console.log(dessert)
-}*/
-
 
 function displaybutton() {
     if (dish!== '' &&  drink!== '' && dessert !== '') {
-        let botao = document.querySelector('.escolher')
-        botao.classList.add('escondido')
-        let botao2 = document.querySelector('.confirmar')
-        botao2.classList.remove('escondido')
+        const botao = document.querySelector('.escolher');
+        botao.classList.add('escondido');
+        const botao2 = document.querySelector('.confirmar');
+        botao2.classList.remove('escondido');
     }
 }
 
 function revisarPedido() {
-    document.querySelector(".prato").innerHTML(dish)
-    document.querySelector(".bebida").innerHTML(drink)
-    document.querySelector(".sobremesa").innerHTML(dessert)
-    document.querySelector(".pratop").innerHTML(pricedish)
-    document.querySelector(".bebidap").innerHTML(pricedrink)
-    document.querySelector(".sobremesap").innerHTML(pricedessert)
+    document.querySelector(".prato").innerHTML(dish);
+    document.querySelector(".bebida").innerHTML(drink);
+    document.querySelector(".sobremesa").innerHTML(dessert);
+    document.querySelector(".pratop").innerHTML(pricedish);
+    document.querySelector(".bebidap").innerHTML(pricedrink);
+    document.querySelector(".sobremesap").innerHTML(pricedessert);
 }
-
-
 
 function finalizarPedido() {
    soma = Number(pricedish) + Number(pricedessert) + Number(pricedrink);
